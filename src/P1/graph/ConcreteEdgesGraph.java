7cb5e6745b01166dcadf3634fcd5d84ff4ef18ea
zhaosuizhi <zhaosuizhi@duck.com>
2021-06-01 22:48:09 +0800

    Finish ConcreteEdgesGraph with tests

--- src/P1/graph/ConcreteEdgesGraph.java
+++ src/P1/graph/ConcreteEdgesGraph.java
@@ -3,11 +3,7 @@
  */
 package P1.graph;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * An implementation of Graph.
@@ -17,45 +13,149 @@ import java.util.Set;
 public class ConcreteEdgesGraph implements Graph<String> {
 
     private final Set<String> vertices = new HashSet<>();
-    private final List<Edge> edges = new ArrayList<>();
+    private final List<Edge<String>> edges = new ArrayList<>();
 
     // Abstraction function:
-    //   TODO
+    //   AF(vertices, edges) = Directed Graph D = (V, E)
+    //       V = vertices
+    //       E = edges
     // Representation invariant:
-    //   TODO
+    //   1. vertices != null && edges != null
+    //   2. for each e in edges, both e.getSource() and e.getTo() in vertices.
+    //   The 1st one is guaranteed by "private final" modifier and "new" syntax on each field,
+    //   so there's no need to check in checkRep method.
     // Safety from rep exposure:
-    //   TODO
+    //   All fields are "private final" so they can't be reassigned
+    //   All fields are mutable, so defensive copy in methods: vertices
 
-    // TODO constructor
-
-    // TODO checkRep
-
-    @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+    private void checkRep() {
+        for (Edge<String> e : edges) {
+            assert vertices.contains(e.getTarget());
+            assert vertices.contains(e.getSource());
+        }
     }
 
-    @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Add a vertex to the graph
+     *
+     * @param vertex label for the new vertex
+     * @return whether vertex had not been on the graph
+     */
+    @Override
+    public boolean add(String vertex) {
+        boolean ret = vertices.add(vertex);
+        checkRep();
+        return ret;
     }
 
-    @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Set a new edge to this graph
+     *
+     * @param source label of the source vertex
+     * @param target label of the target vertex
+     * @param weight non-negative weight of the edge
+     * @return <p> 0 if an new edge was set
+     *         <p> 1 if the edge exists and its weight is update
+     *         <p>-1 if argument error
+     */
+    @Override
+    public int set(String source, String target, int weight) {
+        if (!vertices.contains(source) || !vertices.contains(target))
+            return -1;
+
+        Edge<String> newEdge;
+        try {
+            newEdge = new Edge<>(source, target, weight);
+        } catch (AssertionError e) { // validate error
+            return -1;
+        }
+
+        int ret = 0;
+        for (Edge<String> e : edges) { // remove the old edge
+            if (e.getSource().equals(source) && e.getTarget().equals(target)) {
+                edges.remove(e);
+                ret = 1;
+                break;
+            }
+        }
+        edges.add(newEdge);
+        checkRep();
+        return ret;
     }
 
-    @Override public Set<String> vertices() {
+    /**
+     * Remove the given vertex from this graph
+     *
+     * @param vertex label of the vertex to remove
+     * @return if the vertex was in the graph
+     */
+    @Override
+    public boolean remove(String vertex) {
+        boolean ret = vertices.remove(vertex);
+        if (ret) { // 移出与该顶点有关的所有边
+            edges.removeIf(e -> e.getSource().equals(vertex) || e.getTarget().equals(vertex));
+        }
+        checkRep();
+        return ret;
+    }
+
+    /**
+     * Get all vertices
+     *
+     * @return The set of all vertices
+     */
+    @Override
+    public Set<String> vertices() {
         return new HashSet<>(vertices);
     }
 
-    @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Get all sources of the given target
+     *
+     * @param target a label
+     * @return The map of source->weight
+     */
+    @Override
+    public Map<String, Integer> sources(String target) {
+        Map<String, Integer> src = new HashMap<>();
+        if (vertices.contains(target)) {
+            for (Edge<String> e : edges) {
+                if (e.getTarget().equals(target))
+                    src.put(e.getSource(), e.getWeight());
+            }
+        }
+        return src;
     }
 
-    @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+    /**
+     * Get all targets of the given source
+     *
+     * @param source a label
+     * @return The map of target->weight
+     */
+    @Override
+    public Map<String, Integer> targets(String source) {
+        Map<String, Integer> tar = new HashMap<>();
+        if (vertices.contains(source)) {
+            for (Edge<String> e : edges) {
+                if (e.getSource().equals(source))
+                    tar.put(e.getTarget(), e.getWeight());
+            }
+        }
+        return tar;
     }
 
-    // TODO toString()
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ConcreteEdgesGraph {\n");
+        for (Edge<String> e : edges) {
+            sb.append('\t').append(e).append('\n');
+        }
+        sb.append("}\n");
 
+        return sb.toString();
+    }
 }
 
 /**
@@ -66,23 +166,73 @@ public class ConcreteEdgesGraph implements Graph<String> {
  * <p>PS2 instructions: the specification and implementation of this class is
  * up to you.
  */
-class Edge {
+class Edge<T> {
 
-    // TODO fields
+    private final T source;
+    private final T target;
+    private final int weight;
 
     // Abstraction function:
-    //   TODO
+    //   AF(from, to, weight) = a weighted directed edge from "from" to "to" whose weight is "weight"
     // Representation invariant:
-    //   TODO
+    //   from.equals(to) == false
+    //   weight > 0
     // Safety from rep exposure:
-    //   TODO
+    //   All fields are "private final" so they can't be reassigned
+    //   int is immutable
+    //   source and target are guaranteed by spec since there's no silver bullet for copying generics
 
-    // TODO constructor
+    /**
+     * Create a new Edge
+     * <p>Caller must never mutate "source" and "target" object again!</p>
+     *
+     * @param source source vertex.
+     * @param target target vertex.
+     * @param weight the weight of this edge
+     */
+    public Edge(T source, T target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
 
-    // TODO checkRep
+        checkRep();
+    }
 
-    // TODO methods
+    private void checkRep() {
+        assert !source.equals(target);
+        assert weight > 0;
+    }
 
-    // TODO toString()
 
+    /**
+     * Get the source of the edge
+     *
+     * @return source vertex. <p>Caller must never mutate this T object again!</p>
+     */
+    public T getSource() {
+        return source;
+    }
+
+    /**
+     * Get the target of the edge
+     *
+     * @return target vertex. <p>Caller must never mutate this T object again!</p>
+     */
+    public T getTarget() {
+        return target;
+    }
+
+    /**
+     * Get the weight of the edge
+     *
+     * @return weight
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Edge{%s->%s, w=%d}", source, target, weight);
+    }
 }
--- test/P1/graph/ConcreteEdgesGraphTest.java
+++ test/P1/graph/ConcreteEdgesGraphTest.java
@@ -3,43 +3,231 @@
  */
 package P1.graph;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Tests for ConcreteEdgesGraph.
- * 
+ * <p>
  * This class runs the GraphInstanceTest tests against ConcreteEdgesGraph, as
  * well as tests for that particular implementation.
- * 
+ * <p>
  * Tests against the Graph spec should be in GraphInstanceTest.
  */
 public class ConcreteEdgesGraphTest extends GraphInstanceTest {
-    
+
     /*
      * Provide a ConcreteEdgesGraph for tests in GraphInstanceTest.
      */
-    @Override public Graph<String> emptyInstance() {
+    @Override
+    public Graph<String> emptyInstance() {
         return new ConcreteEdgesGraph();
     }
-    
+
     /*
      * Testing ConcreteEdgesGraph...
      */
-    
+
+    @Test
+    public void testAdd() {
+        Graph<String> graph = emptyInstance();
+        String a = "a";
+        String b = "b";
+
+        Assert.assertTrue(graph.add(a));
+        Assert.assertTrue(graph.add(b));
+        Assert.assertFalse(graph.add(b));
+    }
+
+    @Test
+    public void testSet() {
+        Graph<String> graph = emptyInstance();
+        String a = "a";
+        String b = "b";
+        String c = "c";
+
+        graph.add(a);
+        graph.add(b);
+        int w1 = 3;
+        int w2 = 5;
+
+        Assert.assertEquals(0, graph.set(a, b, w1));
+        Assert.assertEquals(0, graph.set(b, a, w2));
+        Assert.assertEquals(1, graph.set(a, b, w2));
+        Assert.assertEquals(-1, graph.set(b, b, w2));
+        Assert.assertEquals(-1, graph.set(b, c, w2));
+        Assert.assertEquals(-1, graph.set(c, a, w2));
+
+        Map<String, Integer> srcA = graph.sources(a);
+        Assert.assertEquals(1, srcA.size());
+        Assert.assertEquals(w2, (int) srcA.get(b));
+
+        Map<String, Integer> srcB = graph.sources(b);
+        Assert.assertEquals(w2, (int) srcB.get(a));
+    }
+
+    @Test
+    public void testRemove() {
+        Graph<String> graph = emptyInstance();
+        String a = "a";
+        String b = "b";
+        String c = "c";
+
+        graph.add(a);
+        graph.add(b);
+        int w = 5;
+
+        graph.set(a, b, w);
+        graph.set(b, a, w);
+        Assert.assertTrue(graph.remove(a));
+        Assert.assertFalse(graph.remove(c));
+
+        Map<String, Integer> srcB = graph.sources(b);
+        Assert.assertEquals(0, srcB.size());
+    }
+
+    @Test
+    public void testVertices() {
+        Graph<String> graph = emptyInstance();
+        String a = "a";
+        String b = "b";
+        String c = "c";
+
+        graph.add(a);
+        graph.add(b);
+        graph.add(c);
+
+        Set<String> vertices = graph.vertices();
+        Assert.assertEquals(3, vertices.size());
+        Assert.assertTrue(vertices.contains(a));
+        Assert.assertTrue(vertices.contains(b));
+        Assert.assertTrue(vertices.contains(c));
+    }
+
+    @Test
+    public void testSources() {
+        Graph<String> graph = emptyInstance();
+        String a = "a";
+        String b = "b";
+        String c = "c";
+
+        graph.add(a);
+        graph.add(b);
+        graph.add(c);
+        graph.set(b, a, 1);
+        graph.set(c, a, 2);
+
+        Map<String, Integer> src = graph.sources(a);
+        Assert.assertEquals(2, src.size());
+        Assert.assertEquals(1, (int) src.get(b));
+        Assert.assertEquals(2, (int) src.get(c));
+    }
+
+    @Test
+    public void testTargets() {
+        Graph<String> graph = emptyInstance();
+        String a = "a";
+        String b = "b";
+        String c = "c";
+
+        graph.add(a);
+        graph.add(b);
+        graph.add(c);
+        graph.set(b, a, 1);
+        graph.set(c, a, 2);
+
+        Map<String, Integer> src = graph.targets(c);
+        Assert.assertEquals(1, src.size());
+        Assert.assertEquals(2, (int) src.get(a));
+    }
+
     // Testing strategy for ConcreteEdgesGraph.toString()
-    //   TODO
-    
-    // TODO tests for ConcreteEdgesGraph.toString()
-    
+    //   Insert several vertices and edges, then check if the output is correct
+
+    @Test
+    public void testGraphToString() {
+        Graph<String> graph = emptyInstance();
+
+        String a = "a";
+        String b = "b";
+        int w = 2;
+
+        graph.add(a);
+        graph.add(b);
+        graph.set(a, b, w);
+        graph.set(b, a, w);
+
+        System.out.println(graph);
+
+        Assert.assertEquals("ConcreteEdgesGraph {\n" +
+                "\tEdge{a->b, w=2}\n" +
+                "\tEdge{b->a, w=2}\n" +
+                "}\n", graph.toString());
+    }
+
     /*
      * Testing Edge...
      */
-    
+
     // Testing strategy for Edge
-    //   TODO
-    
-    // TODO tests for operations of Edge
-    
+    //    1. Test constructor
+    //        if success when source not equals to target
+    //        if error when source equals to target
+    //        if error when weight == 0
+    //        if error when weight < 0
+    //    2. test all get methods
+    //    3. test toString method
+
+    @Test
+    public void testEdgeConstructor() {
+        String a = "a";
+        String b = "b";
+        String anotherA = "a";
+        Edge<String> edge;
+
+        edge = new Edge<>(a, b, 1);
+
+        try {
+            edge = new Edge<>(a, anotherA, 1);
+            throw new AssertionError();
+        } catch (AssertionError ignored) {
+        }
+
+        try {
+            edge = new Edge<>(a, b, 0);
+            throw new AssertionError();
+        } catch (AssertionError ignored) {
+        }
+
+        try {
+            edge = new Edge<>(a, b, -1);
+            throw new AssertionError();
+        } catch (AssertionError ignored) {
+        }
+    }
+
+    @Test
+    public void testEdgeGet() {
+        String a = "a";
+        String b = "b";
+        int w = 1;
+        Edge<String> edge = new Edge<>(a, b, w);
+
+        Assert.assertEquals(a, edge.getSource());
+        Assert.assertEquals(b, edge.getTarget());
+        Assert.assertEquals(w, edge.getWeight());
+    }
+
+    @Test
+    public void testEdgeToString() {
+        String a = "a";
+        String b = "b";
+        int w = 2;
+        Edge<String> edge = new Edge<>(a, b, w);
+
+        Assert.assertEquals(String.format("Edge{%s->%s, w=%d}", a, b, w), edge.toString());
+    }
 }
